4e0bf05eab01ed4c0a343161769be7cd0db2b522
Arathy <arathyrajeesh2@gmail.com>
2026-01-09 13:36:56 +0530

    view added

--- payslip/models.py
+++ payslip/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    emp_id = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=100)
+    designation = models.CharField(max_length=100)
+    basic_salary = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class Salary(models.Model):
+    employee = models.OneToOneField(Employee, on_delete=models.CASCADE)
+    hra = models.FloatField()
+    da = models.FloatField()
+    allowance = models.FloatField()
+    deductions = models.FloatField()
+    leaves_taken = models.IntegerField(default=0)
+
+    def gross_salary(self):
+        return self.employee.basic_salary + self.hra + self.da + self.allowance
+
+    def net_salary(self):
+        return self.gross_salary() - self.deductions
--- payslip/views.py
+++ payslip/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Employee, Salary
 
-# Create your views here.
+def employee_list(request):
+    employees = Employee.objects.all()
+    return render(request, 'employee_list.html', {'employees': employees})
+
+
+def generate_payslip(request, emp_id):
+    employee = get_object_or_404(Employee, emp_id=emp_id)
+    salary = get_object_or_404(Salary, employee=employee)
+
+    context = {
+        'employee': employee,
+        'salary': salary,
+        'gross_salary': salary.gross_salary(),
+        'net_salary': salary.net_salary()
+    }
+    return render(request, 'payslip.html', context)
