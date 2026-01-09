ea418d6362da09afdd195805151009f5e53dd56c
Arathy <arathyrajeesh2@gmail.com>
2026-01-09 16:17:47 +0530

    Fix static files for Render

--- payslip_project/settings.py
+++ payslip_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ptt(ocg24g5xm5ko*dy^(@3$-5&!d#y5+mg6aw(i-*x0p)k#%q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -118,13 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 # Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'payslip/static',
-]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
